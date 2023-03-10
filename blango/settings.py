d6f6adafaf0444958f16db16d5b20aa9b175e777
olawale oladejo <oladejo61@gmail.com>
2023-03-10 20:59:50 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -147,8 +147,14 @@ class Dev(Configuration):
     "root": {
         "handlers": ["console"],
         "level": "DEBUG",
+        }
     }
-}
+  PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 
 
