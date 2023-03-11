bc273b37550880dfacbd06e469d81c3c173052c4
olawale oladejo <oladejo61@gmail.com>
2023-03-11 20:48:19 +0000

    Finish caching

--- blango/settings.py
+++ blango/settings.py
@@ -149,14 +149,55 @@ class Dev(Configuration):
         "level": "DEBUG",
         }
     }
+
+
+
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+    "require_debug_false": {
+      "()": "django.utils.log.RequireDebugFalse",
+      },
+      },
+    "formatters": {
+      "verbose": {
+        "format": "{levelname} {asctime} {module}\
+        {process:d} {thread:d} {message}",
+        "style": "{",},},
+    "handlers": {"console": {
+        "class": "logging.StreamHandler",
+        "stream": "ext://sys.stdout",
+        "formatter": "verbose",
+        },
+        "mail_admins": {
+          "level": "ERROR",
+          "class": "django.utils.log.AdminEmailHandler",
+          "filters": ["require_debug_false"],
+          },
+          },
+    "loggers": {
+      "django.request": {
+        "handlers": ["mail_admins"],
+        "level": "ERROR",
+        "propagate": True,
+        },
+        },
+    "root": {
+      "handlers": ["console"],
+      "level": "DEBUG",
+      },
+    }
+
   PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-]
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
 
 
 
 class Prod(Dev):
   DEBUG = False
+  SECRET_KEY = values.ListValue()
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,19 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import CommentForm
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
+    
 def post_detail(request, slug):
     if request.user.is_active:
         if request.method == "POST":
